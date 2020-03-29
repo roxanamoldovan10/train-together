0812ec3eb31fcaf485e8aa70849e082cf4dc76ba
BogdanNedelea <live.bobo22@gmail.com>
2020-03-29 20:15:43 +0200

    Add sass variables for colors

--- src/components/dashboard/dashboard.vue
+++ src/components/dashboard/dashboard.vue
@@ -1,2 +1,3 @@
 <template src="./dashboard.html" />
 <script lang="ts" src="./dashboard.ts" />
+<style lang="scss" src="./dashboard.scss" />
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import router from "./router";
 import store from "./store";
 import Buefy from "buefy";
 import "buefy/dist/buefy.css";
+import "@/scss/main.scss";
 
 Vue.use(Buefy);
 Vue.config.productionTip = false;
