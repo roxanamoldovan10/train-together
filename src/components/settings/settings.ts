1b52ead895ad35cfaf1e330c3b53fbffc41b366f
roxanamoldovan10 <roxana.moldovan10@gmail.com>
2020-04-06 19:34:02 +0200

    Updated settings component

--- src/components/settings/settings.ts
+++ src/components/settings/settings.ts
@@ -20,8 +20,14 @@ export default class Settings extends Vue {
     categories: []
   };
   public userUid = "";
+  public categoryUserOptions = {
+    name: "",
+    username: "",
+    gender: "",
+    location: ""
+  };
 
-  public selectedOptions: categoryObject[] = [];
+  public selectedOptions: {}[] = [];
   public updateObject?: any;
 
   // Lifecycle hook
@@ -45,7 +51,7 @@ export default class Settings extends Vue {
   }
 
   /**
-   * REquest for current user deatils
+   * Request for current user deatils
    */
   getUserDetails() {
     const user = firebase.auth().currentUser;
@@ -54,42 +60,44 @@ export default class Settings extends Vue {
       this.usersRef.child(user.uid).once("value", (snapshot: any) => {
         if (snapshot) {
           this.user = snapshot.val();
+          this.categoryUserOptions = {
+            name: this.user.name,
+            username: this.user.username,
+            gender: this.user.gender,
+            location: this.user.location
+          };
           if (this.user.categories) {
-            this.getUserCategories();
+            this.getUserSelectedCategories();
           }
         }
       });
     }
   }
 
-  getUserCategories() {
+  /**
+   * Assign keys for user selected categories
+   */
+  getUserSelectedCategories() {
     const userCategoriesKeys = Object.keys(this.user.categories).map(Number);
     userCategoriesKeys.forEach((categoryKey: number) => {
-      console.log(this.categories, "categories");
-      this.selectedOptions.push(this.categories[categoryKey]);
+      this.selectedOptions.push(categoryKey);
     });
   }
 
-  isActive(category: categoryObject) {
-    const isActive = this.selectedOptions.some(
-      selected => selected === category
-    );
-    return isActive;
-  }
-
   /**
-   * Updates user profile and category table for current user
+   * Updates user profile
+   * @returns {Promise}
    */
-  updateProfile({ username, name, gender, location, categories }: any) {
+  updateProfile({ username, name, gender, location }: any) {
     // aSTEA CE TYPE IS???
     this.updateObject = {};
     this.user.categories = this.user.categories || [];
 
-    const categoryUserOptions = {
-      name: name,
-      username: username,
-      gender: gender,
-      location: location
+    this.categoryUserOptions = {
+      name: this.user.name,
+      username: this.user.username,
+      gender: this.user.gender,
+      location: this.user.location
     };
 
     const userOptions = {
@@ -100,17 +108,13 @@ export default class Settings extends Vue {
       categories: this.user.categories
     };
 
-    if (this.selectedOptions && this.selectedOptions.length) {
-      this.setUserCategory(categoryUserOptions);
-    }
-
     // Bulk update user and categories db objects
     if (this.user.categories) {
       const userCategories = Object.keys(this.user.categories);
       userCategories.forEach(key => {
         this.updateObject[
           `categories/${key}/users/${this.userUid}`
-        ] = categoryUserOptions;
+        ] = this.categoryUserOptions;
       });
     }
     this.updateObject[`users/${this.userUid}`] = userOptions;
@@ -119,21 +123,48 @@ export default class Settings extends Vue {
   }
 
   /**
-   * Inserts selected categories in user db object
-   * Inserts user in selected categories
+   * Add/Remove user category
    */
-  setUserCategory(categoryUserOptions: any) {
-    return this.selectedOptions.forEach((option: any) => {
-      if (typeof option === "string") {
-        this.usersRef
-          .child(this.userUid + "/categories/")
-          .child(option)
-          .set(true);
-        this.categoriesRef
-          .child(option + "/users/" + this.userUid)
-          .set(categoryUserOptions);
-        this.user.categories.push({ option: true });
-      }
+  updateUserCategories(index: string) {
+    const categoryId = parseInt(index);
+    const isSelected = this.selectedOptions.some(selected => {
+      if (typeof selected === "string") selected = parseInt(selected);
+      return selected === categoryId;
     });
+
+    if (isSelected) {
+      return this.addUserCategory(categoryId);
+    }
+    return this.removeUserCategory(categoryId);
+  }
+
+  /**
+   * Add selected category to user
+   * Add user to category
+   * @param categoryId
+   */
+  addUserCategory(categoryId: number) {
+    this.usersRef
+      .child(this.userUid + "/categories/")
+      .child(categoryId)
+      .set(true);
+    this.categoriesRef
+      .child(categoryId + "/users/" + this.userUid)
+      .set(this.categoryUserOptions);
+    this.user.categories.push({ categoryId: true });
+  }
+
+  /**
+   * Removes selected category from user
+   * Removes user from category
+   * @param categoryId
+   */
+  removeUserCategory(categoryId: number) {
+    // messagesRef.child(message.id).remove()
+    this.usersRef
+      .child(this.userUid + "/categories/")
+      .child(categoryId)
+      .remove();
+    this.categoriesRef.child(categoryId + "/users/" + this.userUid).remove();
   }
 }
