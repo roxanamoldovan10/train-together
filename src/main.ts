c9b54f909e179068045dba5664fa8660c6c1d5ed
BogdanNedelea <live.bobo22@gmail.com>
2020-04-09 11:29:26 +0200

    Import font-awesome in the app

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,12 @@ import 'buefy/dist/buefy.css';
 import '@/scss/main.scss';
 import _ from 'lodash';
 import firebaseService from '@/services/firebase-service';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faUserSecret);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.use(Buefy);
 Vue.config.productionTip = false;
