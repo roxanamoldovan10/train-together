cd8c9bb6dda8f965cb1fbaf07106790411a1447e
roxanamoldovan10 <roxana.moldovan10@gmail.com>
2020-04-14 20:03:53 +0200

    Moved firebase calls to services

--- src/services/users-service.ts
+++ src/services/users-service.ts
@@ -1,6 +1,17 @@
 import firebaseConfig from '@/services/firebase-config';
 
 export default class UsersService {
+  public async getCurrentUser(userUid: string) {
+    let data: UserObject[] = [];
+    await firebaseConfig.usersRef
+      .child(userUid)
+      .once('value', (snapshot: any) => {
+        if (snapshot) {
+          data = snapshot.val();
+        }
+      });
+    return data;
+  }
   public createUserProfile(userUid: string, userDetails: UserProfileObject) {
     return firebaseConfig.usersRef.child(userUid).set({
       name: userDetails.name,
@@ -8,6 +19,33 @@ export default class UsersService {
       gender: userDetails.gender,
       location: userDetails.location,
     });
+  }
+
+  public updateBulkUserCategories(
+    userCategories: [],
+    userUid: string,
+    categoryUserOptions: {},
+    userOptions: {},
+  ) {
+    const updateObject: any = {};
+    userCategories.forEach((key: string) => {
+      updateObject[`categories/${key}/users/${userUid}`] = categoryUserOptions;
+    });
+    updateObject[`users/${userUid}`] = userOptions;
+
+    return firebaseConfig.databaseRef.update(updateObject);
+  }
+
+  public addCategoryToUser(userUid: string, categoryId: string) {
+    firebaseConfig.usersRef
+      .child(userUid + '/categories/' + categoryId)
+      .set(true);
+  }
+
+  public removeUserFromCategory(userUid: string, categoryId: string) {
+    firebaseConfig.usersRef
+      .child(userUid + '/categories/' + categoryId)
+      .remove();
   }
 }
 
